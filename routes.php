7e191d84c66f18c243992ebc6544ee363c42ddb4
«MarcLlaoLopez» <mllao@iesebre.com>
2023-11-27 18:38:41 +0100

    Sync

--- routes.php
+++ routes.php
@@ -6,6 +6,7 @@ return [
     '/index'=>'../app/index.php',
     '/home'=>'../app/index.php',
     '/tasks' => '../app/index.php',
+    '/contact' => '../app/contact.php','/about' => '../app/about.php',
 ];
 
 
