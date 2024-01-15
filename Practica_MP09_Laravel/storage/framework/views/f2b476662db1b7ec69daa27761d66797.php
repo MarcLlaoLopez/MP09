971de5854b5eac711540b56befac54e8f364d6ac
«MarcLlaoLopez» <mllao@iesebre.com>
2024-01-15 19:29:35 +0100

    Sync

---
+++ Practica_MP09_Laravel/storage/framework/views/f2b476662db1b7ec69daa27761d66797.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Server Error')); ?>
+<?php $__env->startSection('code', '500'); ?>
+<?php $__env->startSection('message', __('Server Error')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /media/alumne/USB 32GB Marc/SEGUNDO/MP09/Practica_MP09_Laravel/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/500.blade.php ENDPATH**/ ?>
